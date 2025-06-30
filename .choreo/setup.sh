96f0a36053f152571abad5f40db30eed57104b5f
gigara <info@gigara.info>
2025-06-30 11:20:51 +0530

    update ext dir to builtin-extensions-dir

--- .choreo/setup.sh
+++ .choreo/setup.sh
@@ -6,4 +6,4 @@ chmod -R 777 /tmp/chouser
 
 cp -r /home/chouser/. /tmp/chouser/
 
-exec /app/bin/code-server-devant --host 0.0.0.0 --port 8081 --default-folder /tmp/chouser/project --connection-token giga --disable-workspace-trust --extensions-dir /tmp/chouser/.vscode-server-devant/extensions/ --user-data-dir /tmp/chouser/vscode-data
+exec /app/bin/code-server-devant --host 0.0.0.0 --port 8081 --default-folder /tmp/chouser/project --connection-token giga --disable-workspace-trust --builtin-extensions-dir /tmp/chouser/.vscode-server-devant/extensions/ --user-data-dir /tmp/chouser/vscode-data
