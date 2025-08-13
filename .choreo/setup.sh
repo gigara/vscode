f762bf29df29be1658f0b2eff6a46b9e050e3435
gigara <info@gigara.info>
2025-08-13 11:51:14 +0530

    update docker run script for code server

--- .choreo/setup.sh
+++ .choreo/setup.sh
@@ -4,6 +4,43 @@ set -e
 mkdir -p /tmp/chouser
 chmod -R 777 /tmp/chouser
 
+# Git clone logic based on environment variable
+if [ -n "$GIT_CLONE_URL" ]; then
+	echo "Cloning from custom URL: $GIT_CLONE_URL"
+	git clone "$GIT_CLONE_URL" /tmp/bi-project
+	cp -r /tmp/bi-project/. /tmp/chouser/project/
+	rm -rf /tmp/bi-project
+	mkdir -p /tmp/chouser/project/.vscode
+	curl -sSL "https://raw.githubusercontent.com/gigara/ballerina-integrator-empty-proj/refs/heads/main/1.0.0/.vscode/settings.json" -o /tmp/chouser/project/.vscode/settings.json
+	curl -sSL "https://raw.githubusercontent.com/gigara/ballerina-integrator-empty-proj/refs/heads/main/1.0.0/.vscode/launch.json" -o /tmp/chouser/project/.vscode/launch.json
+else
+    echo "Using default repository"
+    git clone https://github.com/gigara/ballerina-integrator-empty-proj.git /tmp/bi-project
+    cp -r /tmp/bi-project/1.0.0/. /tmp/chouser/project/
+    rm -rf /tmp/bi-project
+fi
+
+bal dist use 2201.12.7
+
+git config --global user.name "devant-cloud-editor"
+git config --global user.email "devant-cloud-editor@wso2.com"
+
 cp -r /home/chouser/. /tmp/chouser/
 
-exec /app/bin/code-server-devant --host 0.0.0.0 --port 8081 --default-folder /tmp/chouser/project --connection-token giga --disable-workspace-trust --builtin-extensions-dir /tmp/chouser/.vscode-server-devant/extensions/ --user-data-dir /tmp/chouser/vscode-data
+# Create symlink to mounted extension for live updates
+if [ -d "/mnt/ballerina-extension" ]; then
+	rm -rf /tmp/chouser/.vscode-server-devant/extensions/wso2.ballerina/*
+    ln -sf /mnt/ballerina-extension/* /tmp/chouser/.vscode-server-devant/extensions/wso2.ballerina
+fi
+if [ -d "/mnt/bi-extension" ]; then
+	rm -rf /tmp/chouser/.vscode-server-devant/extensions/wso2.ballerina-integrator/*
+    ln -sf /mnt/bi-extension/* /tmp/chouser/.vscode-server-devant/extensions/wso2.ballerina-integrator
+fi
+if [ -d "/mnt/platform-extension" ]; then
+	rm -rf /tmp/chouser/.vscode-server-devant/extensions/wso2.wso2-platform/*
+    ln -sf /mnt/platform-extension/* /tmp/chouser/.vscode-server-devant/extensions/wso2.wso2-platform
+fi
+
+WORKDIR /tmp/chouser/project/$GIT_SUB_PATH
+
+exec dumb-init /usr/bin/code-server --bind-addr 0.0.0.0:8080 --app-name "Devant Editor" --PASSWORD $CONNECTION_TOKEN --disable-workspace-trust --builtin-extensions-dir /tmp/chouser/.vscode-server-devant/extensions/ --user-data-dir /tmp/chouser/vscode-data /tmp/chouser/project/$GIT_SUB_PATH
